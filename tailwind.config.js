918663f778eecb676bcf1e43b488d797115e40a6
Fabio815 <fabiomotamarques815@gmail.com>
2026-08-14 20:34:51 -0300

    Coloquei o tailwind para funcionar

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [//aquivos que vai ficar observando para gerar o css do tailwind
+    "./src/**/*.{html,ts}", //observando todos os arquivos html e ts dentro da pasta src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
